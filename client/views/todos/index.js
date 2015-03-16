c50dbb3d924027370997c4101c9b7e4a1802a715
AmahAjavon <aj_ajavon@hotmail.com>
2015-03-16 15:27:26 -0700

    functional toto app with firebase and angular

---
+++ client/views/todos/index.js
@@ -0,0 +1,25 @@
+/* global Firebase:true */
+
+'use strict';
+
+angular.module('angular-prototype')
+
+.controller('TodosCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+  var todos = new Firebase('https://ang-fb-todo.firebaseio.com/todos');
+  $scope.todos = $firebaseArray(todos);
+
+  $scope.update = function(todo) {
+    $scope.todos.$save(todo);
+  };
+
+  $scope.add = function() {
+     $scope.todos.$add({
+     title: $scope.todo.title,
+     due: $scope.todo.due.toDateString(),
+     priority: $scope.todo.priority,
+     isComplete: false
+     });
+     $scope.todo = null;
+   };
+
+}]);
